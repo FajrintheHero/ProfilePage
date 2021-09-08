fccf1cd8205d76bcdbdf3e688b26decedfb980dd
FajrintheHero <alamsyahfajrin123@gmail.com>
2021-09-08 23:11:19 +0700

    Tugas 2 Flutter Widget

---
+++ lib/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'widgets/info_card.dart';
+
+const url = "fjralamsyhh.me";
+const email = "fjralam452@gmail.com";
+const phone = "083214436776";
+const location = "Bandung, Indonesia";
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blueGrey[800],
+        body: SafeArea(
+          minimum: const EdgeInsets.only(top: 100),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('../asset/images/profile.png'),
+              ),
+              Text(
+                "Fajrin Alamsyah",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text(
+                "Flutter Developer",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.blueGrey[200],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Source Sans Pro"),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              InfoCard(text: phone, icon: Icons.phone, onPressed: () async {}),
+              InfoCard(text: url, icon: Icons.web, onPressed: () async {}),
+              InfoCard(
+                  text: location,
+                  icon: Icons.location_city,
+                  onPressed: () async {}),
+              InfoCard(text: email, icon: Icons.email, onPressed: () async {}),
+            ],
+          ),
+        ));
+  }
+}
